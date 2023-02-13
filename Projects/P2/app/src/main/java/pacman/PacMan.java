7e173dc3b8ad2dd572c9c4f3720ed287c4fb2e52
Michael Tang <mtangmt@gmail.com>
2023-02-13 15:25:24 -0500

    Removing pacman changes

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import javax.swing.JComponent;
 
-import pacman.Map.Type;
-
 public class PacMan {
   String myName;
   Location myLoc;
@@ -18,25 +16,7 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    ArrayList<Location> result = new ArrayList<Location>();
-    Location possLocation1 = myLoc.shift(0,1);
-    Location possLocation2 = myLoc.shift(0,-1);
-    Location possLocation3 = myLoc.shift(1, 0);
-    Location possLocation4 = myLoc.shift(-1, 0);
-    if (isValidMove(possLocation1)) result.add(possLocation1);
-    if (isValidMove(possLocation2)) result.add(possLocation2);
-    if (isValidMove(possLocation3)) result.add(possLocation3);
-    if (isValidMove(possLocation4)) result.add(possLocation4);
-
-    return result;
-  }
-
-  private boolean isValidMove(Location loc){
-    if (loc.x < 0 || loc.x > 23 || loc.y < 0 || loc.y > 24|| myMap.getField().get(loc).contains(Type.WALL) ) {
-      return false;
-    } else {
-      return true;
-    }
+    return null;
   }
 
   public boolean move() {
