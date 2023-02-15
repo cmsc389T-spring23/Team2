767a863529d51cb8bf00431df3f6fed2c416ff61
J <jasondevers@wireless-10-105-62-208.umd.edu>
2023-02-15 08:19:33 -0500

    finished getLoc(), needs testing

--- Projects/P2/app/src/main/java/pacman/Map.java
+++ Projects/P2/app/src/main/java/pacman/Map.java
@@ -1,7 +1,10 @@
 package pacman;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import javax.swing.JComponent;
+
+import com.sun.jdi.Location;
 
 public class Map {
 
@@ -40,7 +43,8 @@ public class Map {
   public void add(String name, Location loc, JComponent comp, Type type) {
     locations.put(name, loc);
     components.put(name, comp);
-    if (!field.containsKey(loc)) field.put(loc, new HashSet<Type>());
+    if (!field.containsKey(loc))
+      field.put(loc, new HashSet<Type>());
     field.get(loc).add(type);
   }
 
@@ -59,8 +63,16 @@ public class Map {
   }
 
   public HashSet<Type> getLoc(Location loc) {
-    // wallSet and emptySet will help you write this method
-    return null;
+    HashSet<Type> types = field.get(loc);
+    if (types == null) {
+      if (loc.y == 0 || loc.y == dim - 1 || loc.x == 0 || loc.x == dim - 1) {
+        types = new HashSet<>(wallSet);
+      } else {
+        types = new HashSet<>(emptySet);
+      }
+    }
+
+    return types;
   }
 
   public boolean attack(String Name) {
