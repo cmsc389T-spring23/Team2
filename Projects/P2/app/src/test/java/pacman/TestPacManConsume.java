06cae638f979458c414a0615039835ceb5d9403e
tpettwa1 <tpettwa1@terpmail.umd.edu>
2023-02-15 00:20:30 -0500

    Added to the test function.

--- Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
@@ -6,6 +6,14 @@ import java.awt.Color;
 public class TestPacManConsume extends TestCase {
 
   public void testPacManConsume() throws FileNotFoundException {
-    return;
+    //Creating A Map
+    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    //NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+    //Creating Players
+    Ghost ghost = frame.addGhost(new Location(2, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(9, 12)); //Creates PacMan at location x, y
+
+    assertEquals(pacman.consume(), null);
   }
 }
