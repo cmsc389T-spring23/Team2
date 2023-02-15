389f13a352a54915aa051c0cbc040e51d5ef39db
tpettwa1 <tpettwa1@terpmail.umd.edu>
2023-02-15 01:07:42 -0500

    Added test file for the eatCookie method

--- Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
+++ Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
@@ -6,6 +6,15 @@ import java.awt.Color;
 public class TestMapEatCookie extends TestCase {
 
   public void testMapEatCookie() throws FileNotFoundException {
+    //Creating A Map
+    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    //NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+    //Creating Players
+    Ghost ghost = frame.addGhost(new Location(2, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(4, 6)); //Creates PacMan at location x, y
+
+    assertEquals(getMap().eatCookie("pacman"), null);
+    
     return;
   }
 }
