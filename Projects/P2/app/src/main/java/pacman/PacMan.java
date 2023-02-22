c97437a94f884ace1934f6bb7d5ee266f0a7c811
Jason Devers <jdevers1@terpmail.umd.edu>
2023-02-22 08:56:22 -0500

    Revert "Pacman move"

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -1,5 +1,4 @@
 package pacman;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import javax.swing.JComponent;
@@ -21,33 +20,7 @@ public class PacMan {
   }
 
   public boolean move() {
-    ArrayList<Location> validMoves = get_valid_moves();
-    if (validMoves.size() == 0) {
-      return false;
-    } else {
-      HashSet<Location> safeMoves = new HashSet<Location>();
-      for (Location location : validMoves) {
-        PacMan man = new PacMan("", location, myMap);
-        if (man.is_ghost_in_range() == false) {
-          safeMoves.add(location);
-        }
-      }
-
-      if (safeMoves.size() == 0) {
-        Random rando = new Random();
-        int choice = rando.nextInt(validMoves.size());
-        Location finalMove = validMoves.get(choice);
-        myMap.move(myName, finalMove, Map.Type.GHOST);
-      } else {
-        Random rando = new Random();
-        int choice = rando.nextInt(validMoves.size());
-        Location finalMove = validMoves.get(choice);
-        myMap.move(myName, finalMove, Map.Type.PACMAN);
-      }
-
-      return true;
-    }
-
+    return false;
   }
 
   public boolean is_ghost_in_range() {
