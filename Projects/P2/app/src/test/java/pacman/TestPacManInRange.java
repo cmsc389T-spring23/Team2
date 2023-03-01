460e70ee05eb38c0700369ebfc1129a96bd0441c
Wyatt Scott <112585814+wyscott123@users.noreply.github.com>
2023-03-01 15:42:42 -0500

    Updating test to compile and pass
    Think I fixed our issue of it not passing

--- Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManInRange.java
@@ -8,8 +8,8 @@ public class TestPacManInRange extends TestCase {
   public void testPacManInRange() throws FileNotFoundException {
     MainFrame frame = new MainFrame();
     //adding pacman and ghost
-    Ghost ghost = frame.addGhost(new Location(1, 1), "Red", Color.red); //Creates a red ghost named "name" at location x,y
-    PacMan pacman = frame.addPacMan(new Location(0, 1)); //Creates PacMan at location x, y
+    Ghost ghost = frame.addGhost(new Location(4, 5), "Red", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(3, 5)); //Creates PacMan at location x, y
 
     Ghost ghost2 = frame.addGhost(new Location(0, 0), "Blue", Color.blue); //Creates a blue ghost named "Bule" at location x,y
     PacMan pacman2 = frame.addPacMan(new Location(2, 2)); //Creates PacMan at location x, y
