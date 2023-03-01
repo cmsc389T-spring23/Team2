d350e4cc64d0040b94c0eaef45dd66406c6676b1
Wyatt Scott <112585814+wyscott123@users.noreply.github.com>
2023-03-01 15:38:04 -0500

    Adding test to ghost in range
    added test, had it originally dont know why it didn't merge up. Should pass

--- Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
@@ -6,6 +6,16 @@ import java.awt.Color;
 public class TestGhostInRange extends TestCase {
 
   public void testGhostInRange() throws FileNotFoundException {
-    return;
+    MainFrame frame = new MainFrame();
+    //adding pacman and ghost
+    Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(0, 1)); //Creates PacMan at location x, y
+
+    Ghost ghost2 = frame.addGhost(new Location(0, 0), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman2 = frame.addPacMan(new Location(2, 2)); //Creates PacMan at location x, y
+    //should return false
+    assertEquals(pacman2.is_ghost_in_range(), false);
+    //should return true
+    assertEquals(pacman.is_ghost_in_range(), true);  
   }
 }
