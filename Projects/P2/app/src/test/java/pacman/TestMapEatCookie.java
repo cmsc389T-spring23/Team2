a559f0003f4acf1a8d46d665be488eb3cedf585e
tpettwa1 <tpettwa1@terpmail.umd.edu>
2023-02-28 21:29:06 -0500

    Revised Test File

--- Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
+++ Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
@@ -2,19 +2,23 @@ package pacman;
 import java.io.*;
 import junit.framework.*;
 import java.awt.Color;
+import javax.swing.JComponent;
 
 public class TestMapEatCookie extends TestCase {
-
+  
   public void testMapEatCookie() throws FileNotFoundException {
     //Creating A Map
-    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
     //NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
     //Creating Players
     Ghost ghost = frame.addGhost(new Location(2, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
-    PacMan pacman = frame.addPacMan(new Location(4, 6)); //Creates PacMan at location x, y
-
-    assertEquals(getMap().eatCookie("pacman"), null);
+    PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
+    JComponent cookie = frame.getMap().eatCookie("pacman");
+    
+    assertEquals(frame.getMap().getCookies(), 1);
     
     return;
   }
 }
+
+
