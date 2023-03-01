5db292cfde8cc40b44b46309fe259d93d22de59c
Jason Devers <jdevers1@terpmail.umd.edu>
2023-03-01 14:33:17 -0500

    Update TestPacManMove.java

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -6,6 +6,19 @@ import java.awt.Color;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    return;
+    PacMan pacman = new PacMan("pacman", new Location(1, 1), new Map("map1.txt"));
+    pacman.move();
+    assertEquals(pacman.getLoc().x, 1);
+    assertEquals(pacman.getLoc().y, 1);
+    pacman.setLoc(new Location(1, 1));
+    pacman.setDir(0);
+
+    pacman.move();
+    assertEquals(pacman.getLoc().x, 1);
+    assertEquals(pacman.getLoc().y, 0);
+    pacman.setLoc(new Location(1, 1));
+    pacman.setDir(1);
+    pacman.move();
+    assertEquals(pacman.getLoc().x, 2);
   }
 }
