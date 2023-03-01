79f572804e67253147b221385f4ed90f47ff97cf
tpettwa1 <tpettwa1@terpmail.umd.edu>
2023-02-28 22:40:51 -0500

    Added changes to the test function.

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -7,11 +7,11 @@ public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
     //Creating A Map
-    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
     //NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
     //Creating Players
-    Ghost ghost = frame.addGhost(new Location(1, 0), "name", Color.red); //Creates a red ghost named "name" at location x,y
+    Ghost ghost = frame.addGhost(new Location(1, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
     PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
     assertEquals(ghost.attack(), true);
     return;
