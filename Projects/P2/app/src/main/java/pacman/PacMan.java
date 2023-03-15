e030b37e6ccac3a18c8af4877a5f11907101d176
mtangmt <mtangmt@gmail.com>
2023-03-15 12:09:23 -0400

    Revert "adding michael tang sabotage"

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -19,19 +19,20 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    return null;
-    // ArrayList<Location> result = new ArrayList<Location>();
-    // Location possLocation1 = myLoc.shift(0,1);
-    // Location possLocation2 = myLoc.shift(0,-1);
-    // Location possLocation3 = myLoc.shift(1, 0);
-    // Location possLocation4 = myLoc.shift(-1, 0);
-    // if (isValidMove(possLocation1)) result.add(possLocation1);
-    // if (isValidMove(possLocation2)) result.add(possLocation2);
-    // if (isValidMove(possLocation3)) result.add(possLocation3);
-    // if (isValidMove(possLocation4)) result.add(possLocation4);
-    // return result;  
-
-    
+    ArrayList<Location> result = new ArrayList<Location>();
+    Location possLocation1 = myLoc.shift(0, 1);
+    Location possLocation2 = myLoc.shift(0, -1);
+    Location possLocation3 = myLoc.shift(1, 0);
+    Location possLocation4 = myLoc.shift(-1, 0);
+    if (isValidMove(possLocation1))
+      result.add(possLocation1);
+    if (isValidMove(possLocation2))
+      result.add(possLocation2);
+    if (isValidMove(possLocation3))
+      result.add(possLocation3);
+    if (isValidMove(possLocation4))
+      result.add(possLocation4);
+    return result;
   }
 
   private boolean isValidMove(Location loc) {
